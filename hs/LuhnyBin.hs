f2c4851f1057c88d32c04c4789443cec8c883972
Nate Young <nyoung@revelytix.com>
2011-11-16 11:01:40 -0600

    Haskell wip #3

--- hs/LuhnyBin.hs
+++ hs/LuhnyBin.hs
@@ -1,4 +1,4 @@
-import Control.Monad (forever)
+import Control.Monad (forever, msum)
 import Data.Char (isDigit, digitToInt)
 
 digitSum n
@@ -12,22 +12,45 @@ luhnChecksum xs = sum
                          then ((x + s) `mod` 10, n + 1)
                          else (((digitSum $ 2 * x) + s) `mod` 10, n + 1)
 
-readLine line = if and [all isDigit line,
-                        lengthBetween 14 16 line]
-                then Just $ map digitToInt line
-                else Nothing
-    where lengthBetween lo hi x
-              | length x >= lo && length x <= hi = True
-              | otherwise                        = False
+takeCC 0 cs = Just ([],0)
+takeCC _ [] = Nothing
+takeCC n (c:cs)
+    | isDigit c = case takeCC (n - 1) cs of
+                    Just (xs, end) -> Just (x:xs, end+1)
+                    Nothing -> Nothing
+    | c == ' ' || c == '-' = case takeCC n cs of
+                               Just (xs, end) -> Just (xs, end+1)
+                               Nothing -> Nothing
+    | otherwise = Nothing
+    where x = digitToInt c
+
+ccNum input = msum [checkCC 16, checkCC 15, checkCC 14]
+    where checkCC n = case takeCC n input of
+                        Just (digits, end) -> if luhnChecksum digits == 0
+                                              then Just end
+                                              else Nothing
+                        Nothing            -> Nothing
+
+ccWindow [] = Nothing
+ccWindow input = 
+    case ccNum input of
+      Just end -> case ccWindow (tail input) of
+                    Just (_,e) -> Just (0,max end (e+1))
+                    Nothing    -> Just (0,end)
+      Nothing -> case ccWindow (tail input) of
+                   Just (s,e) -> Just (s+1,e+1)
+                   Nothing    -> Nothing
+
+redact line start end = prefix ++ (replicate (end - start) 'X') ++ suffix
+    where (prefix,_) = splitAt start line
+          (_,suffix) = splitAt end line
 
 redactLine :: IO ()
 redactLine = do
   line <- getLine
-  putStrLn $ case readLine line of
-               Just digits -> if (luhnChecksum digits) == 0
-                              then replicate (length line) 'X'
-                              else line
-               Nothing     -> line
+  putStrLn $ case ccWindow line of
+               Just (start, end) -> redact line start end
+               Nothing           -> line
 
 endProgram :: IOError -> IO ()
 endProgram _ = putStrLn ""
